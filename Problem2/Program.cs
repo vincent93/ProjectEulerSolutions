5df816ddf674c22e09dec8f00d68c6c41c0d8bbf
vincent93 <vdegois.ssir@gmail.com>
2018-03-04 20:48:30 +0200

    Restructure sln. Added problem50 (WIP)

---
+++ Problem2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 2;
+            int last = 2;
+            int beforeLast = 1;
+            for (int i = last + beforeLast; i < 4000000; i = last+beforeLast)
+            {
+                beforeLast = last;
+                last = i;
+                if (isEven(i))
+                    sum += i;
+            }
+            Console.WriteLine(sum);
+            Console.Read();
+        }
+
+        public static bool isEven(int n)
+        {
+            return (n % 2 == 0);
+        }
+    }
+}
---
+++ Problem50/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _50ConsecutivePrimeSum
+{
+    class Program
+    {
+        private static int _CAP;
+
+        static void Main(string[] args)
+        {
+            for (int i = primes.Count; i>=0; i--)
+            {
+
+            }
+        }
+
+        static bool canConsecutivePrimeSum(int index)
+        {
+            
+        }
+
+        private static List<int> primes
+        {
+            get
+            {
+                if (primes == null)
+                {
+                    for (int i = 0; i < 1000000; i++)
+                    {
+                        if (isPrime(i))
+                        {
+                            primes.Add(i);
+                        }
+                    }
+                }
+                return primes;
+            }
+        }
+
+        static bool isPrime(int num)
+        {
+            if (num <= 1)
+                return false;
+
+            else if (num <= 3)
+                return true;
+
+            else if (num % 2 == 0 || num % 3 == 0)
+                return false;
+
+            int i = 5;
+            while (i * i <= num)
+            {
+                if (num % i == 0 || num % (i + 2) == 0)
+                    return false;
+
+                i += 6;
+            }
+
+            return true;
+        }
+    }
+}
